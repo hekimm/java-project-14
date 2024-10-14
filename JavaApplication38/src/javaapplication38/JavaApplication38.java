18c5f99920d0f4f37a934c94871e7b534799c772
HEKİMCAN AKTAŞ <hekimcanaktas@gmail.com>
2024-10-14 13:30:20 +0300

    Initial commit

---
+++ JavaApplication38/src/javaapplication38/JavaApplication38.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication38;
+
+/**
+ *
+ * @author hekimcanaktas
+ */
+public class JavaApplication38 {
+public static boolean asalMi(int sayi){
+    for(int i=2;i<sayi/2;i++){
+        if(sayi %i ==0){
+            return false;
+        }
+        
+    }
+   return true; 
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    asalMi(36);
+    }
+    
+}
